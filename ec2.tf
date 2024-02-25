8dd020fc342f5775f681a3759ca1f583a2957a84
03Aziz02 <157199305+03Aziz02@users.noreply.github.com>
2024-02-25 15:30:25 +0530

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "WORDPRESS" {
+  ami           = "ami-0cf7b2f456cd5efd4"
+  instance_type = "t2.micro"
+
+  key_name                    = "new"
+  vpc_security_group_ids      = [aws_security_group.sg1.id]
+  subnet_id                   = aws_subnet.public-subnet1.id
+  associate_public_ip_address = true
+  user_data                   = file("data.sh")
+  tags = {
+    Name = "wordpress"
+  }
+}
