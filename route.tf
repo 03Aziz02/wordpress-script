a6e600e6c87c1595ea45e3af7cda36535f3abd67
03Aziz02 <157199305+03Aziz02@users.noreply.github.com>
2024-02-26 20:32:19 +0530

    Update route.tf

--- route.tf
+++ route.tf
@@ -1,7 +1,19 @@
-resource "aws_route_table" "route1" {
-  vpc_id = aws_vpc.Vpc.id
+# Creating Public Route Table for Demo VPC
+resource "aws_route_table" "public-rt" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = var.anywhere_cidr
+    gateway_id = aws_internet_gateway.igw.id
+  }
 
   tags = {
-    Name = "routetable"
+    Name = "Public Route Table"
   }
+}
+
+# Associating 1st Public Subnet with Public Route Table
+resource "aws_route_table_association" "rt1" {
+  subnet_id      = aws_subnet.subnet-1.id
+  route_table_id = aws_route_table.public-rt.id
 }
