3f218f0bd91edc91588504129737c0355f85613e
03Aziz02 <157199305+03Aziz02@users.noreply.github.com>
2024-02-26 20:28:06 +0530

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -1,32 +1,33 @@
-resource "aws_security_group" "sg1" {
-  vpc_id = aws_vpc.Vpc.id
+# Creating Security Group for EC2 Instance
+resource "aws_security_group" "sg" {
+  vpc_id = aws_vpc.vpc.id
 
+  # Inbound Rules
+  # HTTP access from anywhere
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.anywhere_cidr]
   }
 
+  # SSH access from anywhere
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.anywhere_cidr]
   }
+
+  # Outbound Rules
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.anywhere_cidr]
   }
+
   tags = {
-    Name = "web-sg"
+    Name = "Instance SG"
   }
 }
