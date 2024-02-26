9173274557e6bc04d8fdcb46d5570a53ad2f5d1e
03Aziz02 <157199305+03Aziz02@users.noreply.github.com>
2024-02-26 21:01:41 +0530

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -2,7 +2,7 @@
 #Creating 1st public Subnet
 resource "aws_subnet" "subnet-1" {
   vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.subnet1_cidr
+  cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = "true"
   availability_zone       = "us-east-1a"
   tags = {
