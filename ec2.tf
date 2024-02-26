72d4d480db90dd6e1b404b96b446b3a93b26684e
03Aziz02 <157199305+03Aziz02@users.noreply.github.com>
2024-02-26 21:07:54 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "wordpress" {
   ami                         = "ami-07761f3ae34c4478d"
   instance_type               = "t2.micro"
   key_name                    = "MK"
-  vpc_security_group_ids      = [aws_security_group.ec2-SG.id]
+  vpc_security_group_ids      = [aws_security_group.sg.id]
   subnet_id                   = aws_subnet.subnet-1.id
   associate_public_ip_address = true
   user_data                   = file("data.sh")
