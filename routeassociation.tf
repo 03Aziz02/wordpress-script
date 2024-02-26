3e7b968fe86dee3aeacd66b359d4b5fe8dcd9b77
03Aziz02 <157199305+03Aziz02@users.noreply.github.com>
2024-02-26 20:55:52 +0530

    Update routeassociation.tf

--- routeassociation.tf
+++ routeassociation.tf
@@ -1,4 +1,4 @@
 resource "aws_route_table_association" "route1" {
-  subnet_id      = aws_subnet.public-subnet1.id
-  route_table_id = aws_route_table.route1.id
+  subnet_id      = aws_subnet.subnet-1.id
+  route_table_id = aws_route_table.public-rt.id
 }
