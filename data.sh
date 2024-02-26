fd034303a6d69448fa97d051b64f52fc37bb40c3
03Aziz02 <157199305+03Aziz02@users.noreply.github.com>
2024-02-26 20:28:56 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 sudo yum update -y
-sudo yum -y install docker
+sudo yum install docker -y
 sudo systemctl start docker
 sudo systemctl enable docker
-sudo usermod -a -G ec2-user
 sudo chmod 666 /var/run/docker.sock
-sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-sudo yum -y install git 
+docker-compose version
+pwd
+sudo yum install git -y
 git clone https://github.com/03Aziz02/wordpress.git
 cd wordpress
 docker-compose up -d
